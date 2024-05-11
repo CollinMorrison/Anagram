a1991df89d98c73ce5ecb06b667f3fdfdd5623a2
Collin Morrison <morricollin@gmail.com>
2024-05-10 19:14:25 -0600

    conditional rendering functional

--- webapp/app.vue
+++ webapp/app.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+// state isPlaying controls the game state
+const isPlaying = useState('isPlaying', () => false)
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Word Scramble</h1>
+    <!-- if you're playing the game, render the game -->
+    <div v-if="isPlaying">
+      TEST
+    </div>
+
+    <!-- otherwise, render the welcome content -->
+    <div v-else>
+      <button @click="isPlaying = true">Start Game</button>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+// import WordService from '@/WordService.ts'
+export default {
+  data() {
+    return {
+      scrambledWord: '',
+      word: '',
+      guessedWords: [],
+    }
+  }
+}
+</script>
